88c2e81f41711c258dd92aebca47498630053056
Mwai <machariamwai@gmail.com>
2022-04-26 19:34:09 +0300

    Complete tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    addTask(event.target.addTask.value);
+    form.reset();
+  })
 });
+
+function addTask(event) {
+  let li = document.createElement('li')
+  let deleteButton = document.createElement('button')
+  deleteButton.innerHTML = 'X'
+  deleteButton.style.backgroundColor = 'orange'
+  deleteButton.style.color = 'white'
+  deleteButton.addEventListener('click', handleDelete)
+  li.textContent = `${event} `
+  const parent = document.getElementById('tasks')
+  parent.appendChild(li)
+  li.appendChild(deleteButton)
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
